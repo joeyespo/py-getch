b0d34505195946fe20b2c6ed59981e3531f11e7c
Joe Esposito <joe@joeyespo.com>
2013-01-08 22:16:56 -0500

    Clarify pause docstring.

--- getch/pause.py
+++ getch/pause.py
@@ -2,7 +2,7 @@ from .getch import getch
 
 
 def pause(message='Press any key to continue . . . '):
-    """Prints the message if it's not None and waits for a keypress."""
+    """Prints the specified message if it's not None and waits for a keypress."""
     if message is not None:
         print message,
     getch()
