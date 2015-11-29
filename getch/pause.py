97006f359d323680978a49f78082e64950fa89e0
Joe Esposito <joe@joeyespo.com>
2015-11-29 18:34:46 -0500

    Fix pause() call on Python 2.7.

--- getch/pause.py
+++ getch/pause.py
@@ -9,7 +9,8 @@ def pause(message='Press any key to continue . . . '):
     Prints the specified message if it's not None and waits for a keypress.
     """
     if message is not None:
-        print(message, end='', flush=True)
+        print(message, end='')
+        sys.stdout.flush()
     getch()
     print()
 
